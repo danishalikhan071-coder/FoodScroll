f116e7f79590c1644f662b2c665c0c93458ea4c5
Mohd Danish Ali <danishalikhan071@gmail.com>
2025-11-17 14:16:41 +0000

    Refactor CORS middleware to use environment variable for allowed origins and improve code structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,36 +1,23 @@
-// Server setup
+// create server
 const express = require('express');
-const app = express();
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const foodRoutes = require('./routes/food.routes');
 const foodPartnerRoutes = require('./routes/food-partner.routes');
 const cors = require('cors');
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-// Middleware
-app.use(express.json());
+const app = express();
+app.use(cors({
+    origin: "https://foodscroll.vercel.app",
+    credentials: true
+}));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Test route
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/food', foodRoutes);
 app.use('/api/food-partner', foodPartnerRoutes);
