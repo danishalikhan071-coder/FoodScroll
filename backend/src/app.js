b2f5cbf6409fea7bad37990954f30778cda67c8f
Mohd Danish Ali <danishalikhan071@gmail.com>
2025-11-17 14:07:10 +0000

    Refactor CORS middleware to use setHeader for improved clarity and consistency

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,8 +14,10 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 // Middleware
